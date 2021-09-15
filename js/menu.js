f3bf647f22277ce73b3457cfd13841ce1ad5fe98
xxsimoxx <simone@gieffeedizioni.it>
2021-09-15 11:14:06 +0200

    New icon in v4

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,10 @@
 (function() {
 	tinymce.PluginManager.add('ifcp_mce_menu', function( editor ) {
-	editor.addButton( 'ifcp_mce_menu', {
-		 text: ifcp_mce_menu_name,
-		 icon: 'code',
-		 type: 'menubutton',
-		 menu: ifcp_mce_menu_content
-	});
+		editor.addButton( 'ifcp_mce_menu', {
+			 text: ifcp_mce_menu_name,
+			 icon: 'sharpen',
+			 type: 'menubutton',
+			 menu: ifcp_mce_menu_content
+		});
 	});
 })();
