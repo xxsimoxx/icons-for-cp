dcd049a1719fd71f103e5b3d71936d2cba71001b
xxsimoxx <simospa@gieffeedizioni.it>
2022-05-25 14:54:13 +0200

    Version bump

--- icons-for-cp.php
+++ icons-for-cp.php
@@ -3,7 +3,7 @@
  * Plugin Name: Icons for CP
  * Plugin URI: https://software.gieffeedizioni.it
  * Description: Manage and use SVG icons in your posts and pages.
- * Version: 1.3.0
+ * Version: 1.3.1
  * License: GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Author: Gieffe edizioni srl
